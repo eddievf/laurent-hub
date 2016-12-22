30b4b0eb8c78331a31ec88496db5364457358d50
eddievf <eddievf@hotmail.com>
2016-12-22 12:05:37 -0600

    12.22 - don't want no info
    Small tweaks to the overall code with no significant changes. Removal of 'getinfo' script as the inclusion it's unnecessary

--- filter_maq.php
+++ filter_maq.php
@@ -14,7 +14,6 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<script src="js/scrollspy.js"></script>
-	<script src="js/getinfo.js"></script>
 	<script src="js/getfilters.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.1/js/bootstrap-select.min.js"></script>
 
@@ -92,7 +91,7 @@
 				<div id="results">			
 				<h1 class="page-header">Reporte de Producción y Procesos</h1>
 				
-					<div id="showFilter">
+					<div id="showFilter" class="filterShow">
 					<div class="container-fluid">
 						<div class="row placeholders">
 							
