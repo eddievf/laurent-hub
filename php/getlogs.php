35e639defe4488e024854c3af646d16c70ea2461
eddievf <eddievf@hotmail.com>
2016-12-27 13:42:35 -0600

    12.27 - Pretty Darn Fine, is it not? V2
    # a PDF for them logs, tho
    a) getlogs
    - complete overhaul to identation and function arrangement
    - File no longer ECHOs via a variable, instead echos directly to response text, in compliance with new features
    - addition of object functions to obtain html response from the file. Saved to a variable for use in PDF generator.
    b)pdfLogs
    -New PDF Generator for log files. Follows main template, modifications to headers and titles.

--- php/getlogs.php
+++ php/getlogs.php
@@ -1,101 +1,113 @@
 <?php
-date_default_timezone_set("America/Monterrey");
-$display_string = "<div class='table-responsive'>";
-$display_string .= "<table class='table table-striped table-hover table-bordered table-condensed'>";
-$display_string .= "<thead>";
-$display_string .= "<tr>";
-$display_string .= "<th class = 'col-md-0'>Orden Trabajo</th>";
-$display_string .= "<th class = 'col-md-0'>Partida</th>";
-$display_string .= "<th class = 'col-md-4'>Descripcion</th>";
-$display_string .= "<th class = 'col-md-2'>Proceso Actual</th>";
-$display_string .= "<th class = 'col-md-2'>Proceso Previo</th>";
-$display_string .= "<th class = 'col-md-1'>Avance Actual</th>";
-$display_string .= "<th class = 'col-md-1'>Avance Previo</th>";
-$display_string .= "<th class = 'col-md-2'>Fecha Actualizacion</th>";
-$display_string .= "</tr>";
-$display_string .= "</thead>";
-$display_string .= "<tbody>";
+    date_default_timezone_set("America/Monterrey");
 
-class TableRows extends RecursiveIteratorIterator{
-      function __construct($it){
-         parent::__construct($it, self::LEAVES_ONLY);
-      }
+    class TableRows extends RecursiveIteratorIterator{
+        function __construct($it){
+            parent::__construct($it, self::LEAVES_ONLY);
+        }
+        function current(){
+            return $display_string .="<td>".parent::current()."</td>"; 
+        }
+        function beginChildren(){
+            return $display_string .= "<tr>";
+        }
+        function endChildern(){
+            return $display_string .= "</tr>";
+        }
+    }
 
-      function current(){
-         return $display_string .="<td>".parent::current()."</td>"; 
-      }
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
 
-      function beginChildren(){
-         return $display_string .= "<tr>";
-      }
+    $conn = new PDO("mysql:host=$servername;dbname=test", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-      function endChildern(){
-          return $display_string .= "</tr>";
-      }
-   }
+    // Retrieve data from Query String
+    $order = $_GET['order'];
+    $part = $_GET['part'];
 
-$servername = "localhost";
-$username = "root";
-$password = "";
+    //build query
+    $query = ("SELECT testlog.ID, WorkOrder, WorkDiv, Descripcion, PrevProgress, NewProgress, PrevAvance, NewAvance, ValidUntil
+               FROM testlog, testorders, testpiezas
+               WHERE WorkOrder = :order
+               AND WorkDiv = :part
+               AND WorkOrder = OrdenTrabajo
+               AND WorkDiv = Partida
+               AND testpiezas.ID = testorders.Pieza
+               ORDER BY testorders.ID, ValidUntil DESC, WorkDiv");
 
-$conn = new PDO("mysql:host=$servername;dbname=test", $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $data = $conn->prepare($query);
 
-	
-// Retrieve data from Query String
-$order = $_GET['order'];
-$part = $_GET['part'];
-	
-//build query
-$query = ("SELECT testlog.ID, WorkOrder, WorkDiv, Descripcion, PrevProgress, NewProgress, PrevAvance, NewAvance, ValidUntil
-          FROM testlog, testorders, testpiezas
-          WHERE WorkOrder = :order
-          AND WorkDiv = :part
-          AND WorkOrder = OrdenTrabajo
-          AND WorkDiv = Partida
-          AND testpiezas.ID = testorders.Pieza
-          ORDER BY ValidUntil DESC, WorkDiv");
+    $data->bindParam(':order', $order);
+    $data->bindParam(':part', $part);
+    $data->execute();
 
-$data = $conn->prepare($query);
+    $i=0;
 
-$data->bindParam(':order', $order);
-$data->bindParam(':part', $part);
+    echo '
+        <form action="php/pdfLogs.php" target="_blank" method="POST" id="pdfRequestForm">
+            <button style="margin-top: 5px; margin-bottom: 15px;" type="submit" class="btn btn-danger" name="ReqPDF" id="ReqPDF">
+                <span class="glyphicon glyphicon-save-file"></span> Generar Reporte
+            </button>
 
-$data->execute();
-$i=0;
+    ';
 
-while($row=$data->fetch(PDO::FETCH_ASSOC)){
+    ob_start();
 
-   if($i === 0){
-   $display_string .= "<tr class='info'>";
-   $display_string .= "<td><strong><em>$row[WorkOrder]</em></strong></td>";
-   $display_string .= "<td><strong><em>$row[WorkDiv]</em></strong></td>";
-   $display_string .= "<td><strong><em>$row[Descripcion]</em></strong></td>";
-   $display_string .= "<td class='text-warning'><strong><em>$row[NewProgress]</em></strong></td>";
-   $display_string .= "<td class='text-danger'><strong><em>$row[PrevProgress]</em></strong></td>";
-   $display_string .= "<td class='text-success'><strong><em>$row[NewAvance]%</em></strong></td>";
-   $display_string .= "<td class='text-danger'><strong><em>$row[PrevAvance]%</em></strong></td>";
-   $display_string .= "<td><strong><em>$row[ValidUntil]</em></strong></td>";
-   $display_string .= "</tr>";
-   }
-   else{
-      $display_string .= "<tr>";
-      $display_string .= "<td>$row[WorkOrder]</td>";
-      $display_string .= "<td>$row[WorkDiv]</td>";
-      $display_string .= "<td>$row[Descripcion]</td>";
-      $display_string .= "<td>$row[NewProgress]</td>";
-      $display_string .= "<td>$row[PrevProgress]</td>";
-      $display_string .= "<td>$row[NewAvance]%</td>";
-      $display_string .= "<td>$row[PrevAvance]%</td>";
-      $display_string .= "<td>$row[ValidUntil]</td>";
-      $display_string .= "</tr>";
-   }
+    echo "<div class='table-responsive'>
+     <table class='table table-striped table-hover table-bordered table-condensed'>
+     <thead>
+     <tr>
+     <th class = 'col-md-0'>Orden Trabajo</th>
+     <th class = 'col-md-0'>Partida</th>
+     <th class = 'col-md-4'>Descripcion</th>
+     <th class = 'col-md-2'>Proceso Actual</th>
+     <th class = 'col-md-2'>Proceso Previo</th>
+     <th class = 'col-md-1'>Avance Actual</th>
+     <th class = 'col-md-1'>Avance Previo</th>
+     <th class = 'col-md-2'>Fecha Actualizacion</th>
+     </tr>
+     </thead>
+     <tbody>";
 
-   $i++;
+    while($row=$data->fetch(PDO::FETCH_ASSOC)){
 
-}
+        if($i === 0){
+            echo "<tr class='info'>
+          <td><strong><em>$row[WorkOrder]</em></strong></td>
+          <td><strong><em>$row[WorkDiv]</em></strong></td>
+          <td><strong><em> $row[Descripcion]</em> </strong></td>
+          <td class='text-warning'><strong><em>$row[NewProgress]</em></strong></td>
+          <td class='text-danger'><strong><em>$row[PrevProgress]</em></strong></td>
+          <td class='text-success'><strong><em>$row[NewAvance]%</em></strong></td>
+          <td class='text-danger'><strong><em>$row[PrevAvance]%</em></strong></td>
+          <td><strong><em>$row[ValidUntil]</em></strong></td>
+          </tr>";
+        }
 
-$display_string .= "</tbody></table></div>";
+        else{
+          echo "<tr>
+          <td>$row[WorkOrder]</td>
+          <td>$row[WorkDiv]</td>
+          <td> $row[Descripcion] </td>
+          <td>$row[NewProgress]</td>
+          <td>$row[PrevProgress]</td>
+          <td>$row[NewAvance]%</td>
+          <td>$row[PrevAvance]%</td>
+          <td>$row[ValidUntil]</td>
+          </tr>";
+        }
 
-echo $display_string;
+        $i++;
+    }
+
+    echo "</tbody></table></div>";
+
+    
+    $html = ob_get_contents();
+
+    echo '<input type="hidden" name = "object" value = "'.$html.'" id = "object"> </form>';
+    
+
 ?>
