7afaf555989252fe7baeab8f8f523fa9f2cbe103
eddievf <eddievf@hotmail.com>
2017-01-03 17:25:22 -0600

    01.03 - target acquired
    #MegaFix
    
    New function that allows asynchronous call to validate orders form :)

--- js/getinfo.js
+++ js/getinfo.js
@@ -125,4 +125,29 @@ function showProduct(str){
     }
 }
 
+function showforValid(str){
+   if (str == "") {
+        document.getElementById("valorHint").innerHTML = "";
+        return;
+    }
+    else { 
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        }
+        else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("valorHint").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET","php/getforvalid.php?dart="+str,true);
+        xmlhttp.send();
+    }
+}
+
 
