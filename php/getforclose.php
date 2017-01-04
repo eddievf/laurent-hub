93a2b2c7dbf6dd2b1320c88fc0ff00fc8562fd1f
eddievf <eddievf@hotmail.com>
2017-01-04 17:36:11 -0600

    01.04 - these things happen
    Modifications made to previous file that will upload modifications made on the 'validate div'
    Placeholder file which will update a closing order (Factura and Estado)

---
+++ php/getforclose.php
@@ -0,0 +1,187 @@
+<?php
+    session_start();
+
+    if(!empty($_SESSION['logged'])){
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
+<?php
+$turret = intval($_GET['turret']);
+$table_string = "";
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+$conn = new PDO("mysql:host=$servername;dbname=test", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+//$sql="SELECT * FROM testorders WHERE id = '".$turret."'";
+$query = ("SELECT testorders.ID, OrdenTrabajo, OrdenCompra, testorders.Cliente, Descripcion, FechaSolicitud, Prioridad, ProdKey, Filepath, Partida, Validate
+          FROM testorders, testpiezas
+          WHERE (testpiezas.ID) = (testorders.Pieza)
+          AND OrdenTrabajo = :order
+          ORDER BY Partida");
+
+$data = $conn->prepare($query);
+
+$data->bindParam(':order', $turret);
+$data->execute();
+
+date_default_timezone_set("America/Monterrey");
+$echofun = date("Y-m-d H:i:s");
+
+while($row=$data->fetch(PDO::FETCH_ASSOC)) {
+    $ID = $row['ID'];
+    $OrdenTrabajo = $row['OrdenTrabajo'];
+    $OrdenCompra = $row['OrdenCompra'];
+    $Cliente = $row['Cliente'];
+    $Descripcion = $row['Descripcion'];
+    $FechaSolicitud = $row['FechaSolicitud'];
+    $Prioridad = $row['Prioridad'];
+    $ProdKey = $row['ProdKey'];
+    $Filepath = $row['Filepath'];
+    $Partida = $row['Partida'];
+    $Validate = $row['Validate'];
+
+    $table_string .= "<tr><td>$row[FechaSolicitud]</td>
+                      <td>$row[Descripcion]</td>
+                      <td>$row[Partida]
+                        ";
+    if($Filepath == NULL){
+            $table_string .= "<td><span class = 'label label-danger'><u>Aún no hay dibujo para $row[Descripcion]</u></span></td>";
+        }
+        else{
+            $table_string .= "<td><span class = 'label'><u><a href = '/indevtest/fileHandler.php?file=$row[ProdKey]'>$row[Descripcion]</a></span></u></td>";
+        }
+
+    switch ($Prioridad) {
+        case 0:
+            $table_string .= "<td><span class = 'label label-info'>Normal</span></td>";
+            break;
+        
+        case 1:
+            $table_string .= "<td><span class = 'label label-danger'>Urgente</span></td>";
+            break;
+    }
+
+    switch ($Validate) {
+        case 1:
+            $table_string .= "<td><span class = 'label label-success'><span class = 'glyphicon glyphicon-ok-sign'></span> Liberado</span></td></tr>";
+            break;
+
+        case 2:
+            $table_string .= "<td><span class = 'label label-warning'><span class = 'glyphicon glyphicon-minus-sign'></span> Revision</span></td></tr>";
+            break;
+
+        case 3:
+            $table_string .= "<td><span class = 'label label-danger'><span class = 'glyphicon glyphicon-remove-sign'></span> Pendiente</span></td></tr>";
+            break;
+
+    }
+                    
+}
+
+echo "
+<div class = 'container'>
+    <form class = 'form-horizontal' role = 'form' method = 'POST' action = 'php/validateorder.php' id='valanorder' enctype= 'multipart/form-data'>
+
+    <div class = 'form-group row'>
+        <label for = 'ID' class = 'sr-only'>ID</label>
+        <div class = 'col-sm-7'>
+        <input class = 'form-control' type = 'hidden' value = '".$OrdenTrabajo."' name = 'OrdenFolio' id = 'OrdenFolio'>
+        </div>
+    </div>
+    <div class = 'form-group row'>
+        <label for = 'user' class = 'sr-only'>Usuario Valida</label>
+        <div class = 'col-sm-7'>
+        <input class = 'form-control' type = 'hidden' value= '".$_SESSION['user']."' name = 'UserValid' id = 'UserValid'>
+        </div>
+    </div>
+    <div class = 'form-group row'>
+        <label for = 'DateTime' class = 'col-sm-2'>Hora de Consulta</label>
+        <div class = 'col-sm-7'>
+        <input class = 'form-control' type = 'text' value = '".$echofun."' name = 'DateTime' id = 'DateTime' readonly>
+        </div>
+    </div>
+    <div class = 'form-group row'>
+        <label for = 'OrdenTrabajo' class='col-sm-2 col-form-label'>Orden de Trabajo</label>
+        <div class = 'col-sm-4'>
+        <input class ='form-control' type='number' value='".$OrdenTrabajo."' name = 'OrdenTrabajo' id='OrdenTrabajo'>
+        </div>
+    </div>
+    <div class = 'form-group row'>
+        <label for = 'OrdenCompra' class = 'col-sm-2 col-form-label'>Orden de Compra</label>
+        <div class = 'col-sm-5'>
+        <input class = 'form-control' type='number' value='".$OrdenCompra."' name = 'OrdenCompra' id = 'OrdenCompra'>
+        </div>
+    </div>
+    <div class = 'form-group row'>
+        <label for = 'ValidState' class = 'col-sm-2 col-form-label'>Estado de Orden</label>
+        <div class = 'col-sm-5'>
+                <label class='radio-inline'>
+                    <input type='radio' name='Validate' id='ValidFree' value=1>
+                    <span class = 'label label-success'><span class = 'glyphicon glyphicon-ok-sign'></span> Liberado</span>
+                </label>
+                <label class='radio-inline'>
+                    <input type='radio' name='Validate' id='ValidRev' value=2 checked>
+                    <span class = 'label label-warning'><span class = 'glyphicon glyphicon-minus-sign'></span> Revision</span>
+                </label>
+                <label class='radio-inline'>
+                    <input type='radio' name='Validate' id='ValidError' value=3>
+                    <span class = 'label label-danger'><span class = 'glyphicon glyphicon-remove-sign'></span> Pendiente</span>
+                </label>
+        </div>
+    </div>
+
+    <div class='table-responsive'>
+        <table class='table table-striped table-hover table-bordered'>
+            <thead>
+                <tr>
+                    <th class = 'col-md-2'>Solicitud</th>
+                    <th class = 'col-md-5'>Pieza</th>
+                    <th class = 'col-md-0'>Partida</th>
+                    <th class = 'col-md-3'>Dibujo Actual</th>
+                    <th class = 'col-md-0'>Prioridad</th>
+                    <th class = 'col-md-0'>Liberación</th>
+                </tr>
+            </thead>
+            <tbody>";
+                echo $table_string;   
+            echo "
+            </tbody>
+        </table>
+    </div>
+   
+    <div class = 'form-group row'>
+        <div class = 'offset-sm-2 col-sm-6'>
+        <button type = 'submit' class = 'btn btn-primary'>Actualizar</button>
+        </div>
+    </div>
+    
+    </form>
+</div>
+";
+
+
+
+
+?>
+</body>
+</html>
+
+<?php
+}
+else{
+    echo "not logged in";
+}
+?>
---
+++ php/validateorder.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>[PYMAQ] MAQUINADOS - Confirmación Formularios</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link href="http://localhost/indevdep/css/welcome.css" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="js/scrollspy.js"></script>
+	<script src="js/getinfo.js"></script>
+
+	<?php
+	header("refresh: 3; url= http://localhost/indevdep/forms_maq.php");
+	//database connection
+    try{
+      $servername = "localhost";
+      $username = "root";
+      $password = "";
+
+      $conn = new PDO("mysql:host=$servername;dbname=test", $username, $password);
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+    }
+    catch (PDOException $e){
+			echo "
+			<div class= 'alert alert-danger'><p class='text-center'><strong>[ERROR] </strong> :: <u>".$e->getMessage()."</u> :: (error: JD01)</p></div>";
+    }
+  ?>
+
+</head>
+<body data-spy="scroll" data-target="#side-nav" data-offset="180">
+<!--navbar group-->
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle Navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Forms Maquinados</a>
+			</div>
+			<div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="welcome_rev.php">Inicio</a></li>
+					<li><a href="forms_maq.php">Formularios</a></li>
+					<li><a href="filter_maq.php">Reportes</a></li>
+					<li class="dropdown">
+              			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Departamentos <span class="caret"></span></a>
+              			<ul class="dropdown-menu">
+                			<li><a href="welcome_rev.php">Maquinados</a></li>
+                			<li><a href="#">Almacen</a></li>
+                			<li><a href="#">Troquelado</a></li>
+                			<li role="separator" class="divider"></li>
+                			<li><a href="#">Almacen</a></li>
+                			<li><a href="#">Embarque</a></li>
+              			</ul>
+					</li>
+					<li><a href="#">Help</a></li>
+				</ul>
+			</div><!--collapse-->
+		</div><!--container fluid-->
+	</nav>
+<!--webpage layout-->
+	<div class="container-fluid">
+		<div class="row">
+			<!--sidebar-->
+			<div class="col-sm-3 col-md-2 sidebar affix" id="side-nav">
+				<ul class="nav nav-sidebar">
+					<li><a href="#successpanel">Actualizado</a></li>
+					<li><a href="#dangerpanel">Error</a></li>
+			</div>
+			<!--end sidebar-->
+			<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+			<!--Main Content-->	
+
+			<?php
+
+				date_default_timezone_set("America/Monterrey");
+				$echofun = date("Y-m-d H:i:s");
+
+
+				$servername = "localhost";
+				$username = "root";
+				$password = "";
+
+				try{
+					$conn = new PDO("mysql:host=$servername;dbname=test", $username, $password);
+
+					$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+					$OrdenFolio = $_POST['OrdenFolio'];
+					$UserValid = $_POST['UserValid'];
+					$FreeDate = $_POST['DateTime'];
+					$OrdenTrabajo = $_POST['OrdenTrabajo'];
+					$OrdenCompra = $_POST['OrdenCompra'];
+					$Validate = $_POST['Validate'];
+
+					if($Validate == 1){
+						$stmt = $conn->prepare("UPDATE testorders
+												SET OrdenTrabajo = :OrdenTrabajo, OrdenCompra = :OrdenCompra, Validate = :Validate, UserValid = :UserValid, FreedomDate = :FreeDate
+												WHERE OrdenTrabajo = :OrdenFolio");
+					}
+
+					else{
+						$stmt = $conn->prepare("UPDATE testorders
+												SET OrdenTrabajo = :OrdenTrabajo, OrdenCompra = :OrdenCompra, Validate = :Validate
+												WHERE OrdenTrabajo = :OrdenFolio");
+					}
+
+					$stmt->bindParam(':OrdenTrabajo', $OrdenTrabajo);
+					$stmt->bindParam(':OrdenCompra', $OrdenCompra);
+					$stmt->bindParam(':Validate', $Validate);
+					$stmt->bindParam(':OrdenFolio', $OrdenFolio);
+
+					if($Validate == 1){
+						$stmt->bindParam(':UserValid', $UserValid);
+						$stmt->bindParam(':FreeDate', $FreeDate);
+
+					}
+					
+
+				?>
+				<div class="panel panel-default" id="success">
+					<div class="panel-heading">
+						<h3 class="panel-title">Se ha recibido la siguiente información</h3>
+					</div>
+					<div class="panel-body">
+						<ul class="list-group">
+				<?php
+
+					echo "<li class='list-group-item'><strong>Orden Registrada</strong>: ".$OrdenTrabajo."</li>";
+					echo "<li class='list-group-item'><strong>Orden Compra</strong>: ".$OrdenCompra."</li>";
+					echo "<li class='list-group-item'><strong>Estado Actual</strong>: ".$Validate."</li>";
+					if($Validate == 1){
+						echo "<li class='list-group-item'><strong>Usuario que Libera: </strong>".$UserValid."</li>";
+						echo "<li class='list-group-item'><strong>Fecha de Liberacion: </strong>".$FreeDate."</li>";
+
+					}
+					echo "<li class='list-group-item'><strong>Orden Folio</strong>: ".$OrdenFolio."</li></ul>
+					</div>
+				</div>
+					";
+
+					$stmt->execute();
+					echo "
+					<div id= 'successpanel' class= 'panel panel-success'>
+						<div class='panel-heading'>
+							<h3 class='panel-title'><strong>[SUCCESS]</strong> :: Actualizacion Exitosa</h3>
+						</div>
+						<div class='panel-body'>
+							<p><strong>[PRODUCT]</strong> :: <u>La información del producto ha sido actualizada con la información más reciente</u></p>
+						</div>
+					</div>";
+
+					
+	
+				}
+
+				catch (PDOException $e){
+					echo "
+						<div id='dangerpanel' class='panel panel-danger'>
+							<div class= 'panel-heading'>
+								<h3 class='panel-title'><strong>[ERROR]</strong> :: Actualizacion no completada</h3>
+							</div>
+							<div class= 'panel-body'>
+								<p><strong><span class='text-danger'>[JD04]</span></strong> :: <u>:".$e->getMessage()."</u><br>
+								<span class= 'text-danger'>Verificar la conexión a la base de datos y que <u>todos</u> los campos hayan sido actualizados.</span></p>
+							</div>
+						</div>
+						";				
+				}
+
+
+				?>
+
+
+
+			</div><!--Main Content until here-->	
+
+
+
+		</div>
+	</div>
+
+</body>
+</html>
