55f9d8b91ebcdd5dc7177a2c07482a41794df6ec
eddievf <eddievf@hotmail.com>
2017-01-06 12:53:34 -0600

    01.06 - [computer gibberish]
    #Sessions
    
    Modifications made to 'revstatus' to add protection against unwanted query.
    Addition of new 'notfound' page to standarize output for not logged in pages
    Modifications to 'logout' page's title

--- notfound.php
+++ notfound.php
@@ -1,17 +1,13 @@
 <?php
-	session_start();
-	session_destroy();
-
 	header("refresh: 3; url= http://localhost/indevdep/index.html");
-
-	echo '
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Sesión Terminada</title>
+	<title>oh hi there</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.1/css/bootstrap-select.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
@@ -36,8 +32,8 @@
 
 	<div class="jumbotron jumbotron-fluid">
 		<div class="container">
-			<h1><u>Sesión Terminada</u></h1>
-			<p>La sesión actual ha sido terminada.<br>Puede volver a ingresar con sus mismas credenciales desde el portal de acceso.<br>A continuación, será redirigido al sitio de inicio de sesión.</p><br>
+			<h1><u>404</u></h1>
+			<p>¿Te has perdido?<br>¿Desapareció el sitio que buscabas?<br>A continuación, será redirigido al que querias llegar.</p><br>
 			<p><small>Dar click <u><a href="http://localhost/indevdep/index.html">aquí</a></u> si no se redirige automaticamente.</small></p>
 		</div>
 	</div>
@@ -46,5 +42,5 @@
 		<p class="text-muted"><small>2017 Prensas y Maquinados SA de CV.<br>Contact webmaster at info@eddievf.com</small></p>
 	</div>
 </body>
-</html>
-';
+</html>';
+}
