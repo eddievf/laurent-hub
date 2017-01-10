ec64001b459d5b991cdb87421b449c9bf4618842
eddievf <eddievf@hotmail.com>
2017-01-10 11:59:29 -0600

    01.10 - differences behind us
    #INVOICE
    
    -Modifications to GUI. User Interface now displays information in panels, including both Success and Danger Panels, with a yellow Warning Panel for deliberate empty fields

--- php/bagtagit.php
+++ php/bagtagit.php
@@ -12,15 +12,15 @@ if(!empty($_SESSION['logged'])){
 	<meta name="viewport" content="widht device-width, initial-scale=1">
 	<title>[PYMAQ] MAQUINADOS - Confirmar Formularios</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link href="http://localhost/indevdep/css/welcome.css" rel="stylesheet">
+	<link href="../css/welcome.css" rel="stylesheet">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<script src="js/scrollspy.js"></script>
-	<script src="js/getinfo.js"></script>
+	<script src="../js/scrollspy.js"></script>
+	<script src="../js/getinfo.js"></script>
 
 	<?php
 		date_default_timezone_set("America/Monterrey");
-		//header("refresh: 5; url=http://localhost/indevdep/forms_maq.php");
+		//header("refresh: 13; url=http://localhost/indevdep/forms_maq.php");
 
 		try{
 			$servername = "localhost";
@@ -68,7 +68,7 @@ if(!empty($_SESSION['logged'])){
                 			<li><a href="#">Embarque</a></li>
               			</ul>
 					</li>
-					<li><a href="#">Help</a></li>
+					<li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Salir</a></li>
 				</ul>
 			</div><!--collapse-->
 		</div><!--container fluid-->
@@ -79,6 +79,7 @@ if(!empty($_SESSION['logged'])){
 			<!--sidebar-->
 			<div class="col-sm-3 col-md-2 sidebar affix" id="side-nav">
 				<ul class="nav nav-sidebar">
+					<li><a href="#infopanel">Información</a></li>
 					<li><a href="#successpanel">Actualizado</a></li>
 					<li><a href="#dangerpanel">Error</a></li>
 			</div>
@@ -90,8 +91,6 @@ if(!empty($_SESSION['logged'])){
 				/*GettingVariables*/
 					$OrdenCompra = $_POST['OrdenCompra'];
 					$UserValid = $_POST['UserValid'];
-
-					echo "<br>".$UserValid."<br>";
 					$DateTime = $_POST['DateTime'];
 					$OrdenTrabajo = $_POST['OrdenTrabajo'];
 					$Cliente = $_POST['Cliente'];
@@ -116,7 +115,6 @@ if(!empty($_SESSION['logged'])){
 						$part = $row['Partida'];
 						$ID[$part] = $row['ID'];
 						$WorkOrder[$part] = $row['OrdenTrabajo'];
-						echo "WorkOrder (".$part.") es igual a ".$WorkOrder[$part]." <br>";
 						$CantidadTotal[$part] = $row['Cantidad'];
 						$CantidadPendiente[$part] = $row['CantPending'];
 						$Pieza[$part] = $row['Pieza'];
@@ -126,113 +124,127 @@ if(!empty($_SESSION['logged'])){
 						$PrevAvance[$part] = $row['Avance'];
 					}
 
-					for ($i=1; $i <= $part ; $i++) { 
-						echo	"<br>Contenido:
-							<br>Part = ".$i."
-							<br>ID(".$i.") = ".$ID[$i]."
-							<br>OrdenTrabajo(".$i.") = ".$WorkOrder[$i]."
-							<br>CantidadTotal(".$i.") = ".$CantidadTotal[$i]."
-							<br>CantidadPendiente(".$i.") = ".$CantidadPendiente[$i]."
-							<br>Pieza(".$i.") = ".$Pieza[$i]."
-							<br>Factura(".$i.") = ".$LoggedFactura[$i]."
-							<br>End<br>";
+			?>
 
-						if($CantidadPendiente[$i] != 0 ){
+			<?php
+				echo "<div id='infopanel'></div>";
 
-							$postCant = "RealCant".$ID[$i];
-							$postFact = "Factura".$ID[$i];
-							$postIDOr = "OrdenID".$ID[$i];
+				for ($i=1; $i <= $part ; $i++) { 
 
-							$RealCant[$i] = $_POST[$postCant];
-							$Factura[$i] = $_POST[$postFact];
-							$OrdenID[$i] = $_POST[$postIDOr];
+					if($CantidadPendiente[$i] != 0 ){
+						echo "<div class='panel panel-default' id='success'>
+								<div class='panel-heading'>
+									<h3 class='panel-title'>Información de Partida [".$i."]</h3>
+								</div>
+								<div class='panel-body'>
+									<ul class='list-group'>";
 
-							echo "<br>VARS<br>
-							<br>RealCant(".$i.") = ".$RealCant[$i]."
-							<br>Factura(".$i.") = ".$Factura[$i]."
-							<br>OrdenID(".$i.") = ".$OrdenID[$i]."
-							<br><br> END VARS <br>
+						$postCant = "RealCant".$ID[$i];
+						$postFact = "Factura".$ID[$i];
+						$postIDOr = "OrdenID".$ID[$i];
 
+						$RealCant[$i] = $_POST[$postCant];
+						$Factura[$i] = $_POST[$postFact];
+						$OrdenID[$i] = $_POST[$postIDOr];
+
+						echo "<li class='list-group-item'><strong>Cantidad Facturada</strong>:  ".$RealCant[$i]."</li>";
+						echo "<li class='list-group-item'><strong>Folio de Factura</strong>: ".$Factura[$i]."</li>";
+						echo "<li class='list-group-item'><strong>Orden de Trabajo</strong>: ".$WorkOrder[$i]."</li>
+									</ul>
+								</div>
+							  </div>";
+
+						$cantResult[$i] = $CantidadPendiente[$i] - $RealCant[$i];
+
+						if($cantResult[$i] == $CantidadPendiente[$i]){
+							echo "
+							<div id='warningpanel' class='panel panel-warning'>
+								<div class= 'panel-heading'>
+									<h3 class='panel-title'><strong>[Alert]</strong> :: Sin información para actualizar</h3>
+								</div>
+								<div class= 'panel-body'>
+									<p><strong><span class='text-warning'>[JD12-1]</span></strong> :: <u>Cantidad Pendiente sin cambios</u><br>
+									No se han indicado piezas a facturar. La Partida no será actualizada.</p>
+								</div>
+							</div>
 							";
+						}
 
-							$cantResult[$i] = $CantidadPendiente[$i] - $RealCant[$i];
+						if($cantResult[$i] < 0){
+							echo "
+								<div id='dangerpanel' class='panel panel-danger'>
+									<div class= 'panel-heading'>
+										<h3 class='panel-title'><strong>[ERROR]</strong> :: Actualizacion no completada</h3>
+									</div>
+									<div class= 'panel-body'>
+										<p><strong><span class='text-danger'>[JD12-2]</span></strong> :: <u>Cantidad Pendiente resulta menor a 0</u><br>
+										<span class= 'text-danger'>La cantidad de piezas a facturar supera la cantidad de Pendientes registrados.<br>
+										Vuelva al formulario y reingrese la información correctamente.</span></p>
+									</div>
+								</div>
+								";
+							exit();
 
-							if($cantResult[$i] == $CantidadPendiente[$i]){
-								echo "Error, Nothing sent to Database as Pendings remain the same. Thank You<br>";
-							}
-
-							if($cantResult[$i] < 0){
-								echo "Something Happened";
-								exit();
-
-							}
-							else{
-
-								if($cantResult[$i] == 0){
-									$NewProgress[$i] = "Facturacion - ".$Factura[$i].". (Final)";
-									$PartialState[$i] = 1;
-
-								}
-								else{
-									$NewProgress[$i] = "Facturacion - ".$Factura[$i].". (Parcial)";
-									$PartialState[$i] = 0;
-								}
-
-								if($LoggedFactura[$i] == 0){
-									$FactString[$i] = $Factura[$i];
-								}
-								else{
-									$FactString[$i] .= ", ".$Factura[$i];
-								}
-								$stmt->bindParam(':cantpending', $cantResult[$i]);
-
-								if($cantResult[$i] == $CantidadPendiente[$i]){
-									$FactString[$i] = $LoggedFactura[$i];
-									$NewProgress[$i] = $Progress[$i];
-								}
-
-								$stmt->bindParam(':factura', $FactString[$i]);
-								$stmt->bindParam(':partial', $PartialState[$i]);
-								$stmt->bindParam(':progress', $NewProgress[$i]);
-								$stmt->bindParam(':OrdenID', $OrdenID[$i]);
-								$stmt->execute();
-								echo "woohoo ".$i."<br>";
-
-								$log->bindParam(':OrdenTrabajo', $WorkOrder[$i]);
-								$log->bindParam(':Partida', $i);
-								$log->bindParam(':PrevProgress',$Progress[$i]);
-								$log->bindParam(':NewProgress', $NewProgress[$i]);
-								$log->bindParam(':PrevAvance', $PrevAvance[$i]);
-								$log->bindParam(':NewAvance', $PrevAvance[$i]);
-								$log->bindParam(':ValidUntil', $DateTime);
-								$log->execute();
-								echo "hooray ".$i."<br>";
-							}
 						}
 
 						else{
-							echo "<br>No Pending Products<br><br>";
+
+							if($cantResult[$i] == 0){
+								$NewProgress[$i] = "Facturacion - ".$Factura[$i].". (Final)";
+									$PartialState[$i] = 1;
+							}
+							else{
+								$NewProgress[$i] = "Facturacion - ".$Factura[$i].". (Parcial)";
+								$PartialState[$i] = 0;
+							}
+
+
+							if($LoggedFactura[$i] == 0){
+								$FactString[$i] = $Factura[$i];
+							}
+							else{
+								$FactString[$i] .= ", ".$Factura[$i];
+							}
+
+
+							$stmt->bindParam(':cantpending', $cantResult[$i]);
+
+							if($cantResult[$i] == $CantidadPendiente[$i]){
+								$FactString[$i] = $LoggedFactura[$i];
+								$NewProgress[$i] = $Progress[$i];
+							}
+
+							$stmt->bindParam(':factura', $FactString[$i]);
+							$stmt->bindParam(':partial', $PartialState[$i]);
+							$stmt->bindParam(':progress', $NewProgress[$i]);
+							$stmt->bindParam(':OrdenID', $OrdenID[$i]);
+							$stmt->execute();
+							echo "
+								<div id= 'successpanel' class= 'panel panel-success'>
+									<div class='panel-heading'>
+										<h3 class='panel-title'><strong>[SUCCESS]</strong> :: Actualizacion Exitosa</h3>
+									</div>
+									<div class='panel-body'>
+										<p><strong>[PRODUCT]</strong> :: <u>La partida (".$WorkOrder[$i]." - ".$i.") ha sido actualizada con la información más reciente</u></p><br>
+										<p><strong>[LOG]</strong> :: <u>La información del Proceso ha sido registrada</u></p>
+									</div>
+								</div>
+								<hr style='color: black; height: 1px; background-color:black;' />
+							";
+
+							$log->bindParam(':OrdenTrabajo', $WorkOrder[$i]);
+							$log->bindParam(':Partida', $i);
+							$log->bindParam(':PrevProgress',$Progress[$i]);
+							$log->bindParam(':NewProgress', $NewProgress[$i]);
+							$log->bindParam(':PrevAvance', $PrevAvance[$i]);
+							$log->bindParam(':NewAvance', $PrevAvance[$i]);
+							$log->bindParam(':ValidUntil', $DateTime);
+							$log->execute();
 						}
-
-						
-
-						
-
 					}
-					
+				}
+			?>
 
-
-				?>
-
-			</div><!--Main Content until here-->	
-
-
-
-		</div>
-	</div>
-
-</body>
-</html>
 <?php
 	}
 	else{
